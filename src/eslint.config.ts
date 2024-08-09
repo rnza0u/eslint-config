12f7efee3ed7238193898f40234534b925d50249
rnzaou <nzaou.renaud@gmail.com>
2024-08-09 13:14:38 +0200

    init

---
+++ src/eslint.config.ts
@@ -0,0 +1,45 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
+import _globals from 'globals'
+
+export default tseslint.config(
+    {
+        ...eslint.configs.recommended,
+        languageOptions: {
+            globals: {
+                ..._globals.browser,
+                ..._globals.node
+            },
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true
+                }
+            }
+        }
+    },
+    ...tseslint.configs.recommended,
+    ...tseslint.configs.strict,
+    {
+        rules: {
+            '@typescript-eslint/no-var-requires': 'off'
+        }
+    },
+    {
+
+        plugins: {
+            /* eslint-disable */
+            '@stylistic': stylistic as any
+            /* eslint-enable */
+        },
+        rules: {
+            '@stylistic/indent': ['error', 4],
+            '@stylistic/no-extra-semi': ['error'],
+            '@stylistic/semi': ['error', 'never'],
+            '@stylistic/quotes': ['error', 'single']
+        }
+    },
+    {
+        ignores: ['**/dist/', '**/lib/', '**/node_modules', 'build/', '.docusaurus/'],
+    }
+)
---
+++ workspace.jsonnet
@@ -0,0 +1,5 @@
+{
+    projects: {
+        'eslint-config': ''
+    }
+}
